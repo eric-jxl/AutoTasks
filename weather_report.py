823ff6ca98da85248c968be1cd2d4ee9f4a1e188
Eric <799050481@qq.com>
2025-02-24 11:57:59 +0800

    修改天气接口
    Signed-off-by: Eric <799050481@qq.com>

--- weather_report.py
+++ weather_report.py
@@ -9,43 +9,18 @@ appID = os.environ.get("APP_ID")
 appSecret = os.environ.get("APP_SECRET")
 openId = os.environ.get("OPEN_ID")
 weather_template_id = os.environ.get("TEMPLATE_ID")
+AMP_KEY = os.environ.get("AMP_KEY")
 
-weather_cache = {}
 
+def get_weather(city):
 
-def get_weather(my_city):
-    if my_city in weather_cache:
-        return weather_cache[my_city]
-
-    url = "https://www.weather.com.cn/textFC/{}.shtml"
-    regions = ["hb", "db", "hd", "hz", "hn", "xb", "xn"]
-
-    for region in regions:
-        resp = requests.get(url.format(region))
-        soup = BeautifulSoup(resp.content, 'html5lib')
-        tables = soup.select("div.conMidtab table")
-
-        for table in tables:
-            for tr in table.find_all("tr")[2:]:
-                tds = tr.find_all("td")
-                city_td = tds[-8]
-                this_city = city_td.get_text(strip=True)
-
-                if this_city == my_city:
-                    high_temp = tds[-5].get_text(strip=True)
-                    low_temp = tds[-2].get_text(strip=True)
-                    weather_typ_day = tds[-7].get_text(strip=True)
-                    weather_type_night = tds[-4].get_text(strip=True)
-                    wind_day = " ".join(tds[-6].stripped_strings)
-                    wind_night = " ".join(tds[-3].stripped_strings)
-
-                    temp = f"{low_temp}~{high_temp}摄氏度" if high_temp != "-" else f"{low_temp}摄氏度"
-                    weather_typ = weather_typ_day if weather_typ_day != "-" else weather_type_night
-                    wind = wind_day if wind_day != "--" else wind_night
-
-                    weather_cache[my_city] = (
-                        this_city, temp, weather_typ, wind)
-                    return this_city, temp, weather_typ, wind
+    url = f"https://restapi.amap.com/v3/weather/weatherInfo"
+    req = requests.get(
+        url, parms={"key": AMP_KEY, "city": city, "output": "json"})
+    if req.status_code != 1:
+        weather_logger.error(
+            f"Request failed with status code {req.status_code}")
+    return req.json().get('lives')[0]
 
 
 def get_access_token():
@@ -61,7 +36,7 @@ def get_daily_love():
     return sentence
 
 
-def send_weather(access_token, weather):
+def send_weather(access_token, weather: dict):
     import datetime
     today_str = datetime.date.today().strftime("%Y年%m月%d日")
     for oid in openId.split(','):
@@ -70,10 +45,10 @@ def send_weather(access_token, weather):
             "template_id": weather_template_id.strip(),
             "data": {
                 "date": {"value": today_str},
-                "region": {"value": weather[0]},
-                "weather": {"value": weather[2]},
-                "temp": {"value": weather[1]},
-                "wind_dir": {"value": weather[3]},
+                "region": {"value": weather.get('province') + weather.get('city')},
+                "weather": {"value": weather.get('weather')},
+                "temp": {"value": weather.get('temperature')},
+                "wind_dir": {"value": f"{weather.get('winddirection')}风，风力{weather.get('windpower')}级,湿度:{weather.get('humidity_float')}"},
                 "today_note": {"value": get_daily_love(), "color": "#d76c25"},
             }
         }
