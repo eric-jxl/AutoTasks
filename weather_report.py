c60240238a4d0f1833c9cd1b7d530f11f5f2e7d7
Eric <45086115+eric-jxl@users.noreply.github.com>
2025-03-18 10:38:46 +0800

    Update weather_report.py

--- weather_report.py
+++ weather_report.py
@@ -1,11 +1,9 @@
 import os
-import sys
 import requests
 import logging
 from bs4 import BeautifulSoup
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from .utils import retry_on_exception
+from utils import retry_on_exception
 
 weather_logger = logging.getLogger("weather")
 
